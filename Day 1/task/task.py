4dd555179c6264404485f2ee13633b9cf7d81ff2
arslanmit <arslanmit@gmail.com>
2024-11-08 19:56:31 +0100

    1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,5 @@
-name = "Angela"
-length = len(name)
-print(length)
+print("Welcome to the Band Name Generator.")
+city=input("What's the name of the city you grew up in?\n")
+print(city)
+pet=input("What's your pet's name?\n")
+print("Your band name could be "+city+" "+pet)
