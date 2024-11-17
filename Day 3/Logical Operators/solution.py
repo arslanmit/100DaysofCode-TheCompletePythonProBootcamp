be8dabac25996f433231d6a2f714be3139261198
Umit Arslan <arslanmit@gmail.com>
2024-11-18 00:36:49 +0100

    3 end

--- Day 3/Logical Operators/solution.py
+++ Day 3/Logical Operators/solution.py
@@ -12,15 +12,13 @@ if height >= 120:
         bill = 7
         print("Youth tickets are $7.")
     elif age >= 45 and age <= 55:
-        # Or
-        # 45 <= age <= 55
         print("Everything is going to be ok. Have a free ride on us!")
     else:
         bill = 12
         print("Adult tickets are $12.")
 
     wants_photo = input("Do you want a photo taken? Y or N. ")
-    if wants_photo == "Y":
+    if wants_photo.upper() == "Y":
         bill += 3
 
     print(f"Your final bill is ${bill}")
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,9 @@
+numbersx = int(input('Enter '))
+
+if numbersx % 2 == 0:
+    print('Even')
+    if numbersx <= 18:
+            print("Please pay $7.")
+
+else :
+    print('Odd')
