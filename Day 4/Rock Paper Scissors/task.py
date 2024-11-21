4ab9a8480d63998114df34649e93bd6d1b43dacd
Umit Arslan <arslanmit@gmail.com>
2024-11-21 20:53:37 +0100

    4 finished

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -27,13 +27,31 @@ scissors = '''
 ---.__(___)
 '''
 
-game=[rock,paper,scissors]
+game_images = [rock, paper, scissors]
 
-user_choice=int(input('0 Rock, 1 Paper,2, Scissors\n'))
+user_choice = int(input(" 0 for Rock, "
+                        "1 for Paper or "
+                        "2 for Scissors.\n"))
 
-print(print(game[user_choice]))
-computer_choice=random.randint(0,2)
 
-print(computer_choice)
-print(game[computer_choice])
 
+if 0 <= user_choice  and user_choice <= 2:
+    print(game_images[user_choice])
+print("-" * 30)
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print("-" * 30)
+print(game_images[computer_choice])
+print("-" * 30)
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw!")
